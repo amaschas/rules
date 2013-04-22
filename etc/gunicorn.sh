49f2f9513bb00722795c742f72ea57ea7585574d
Alexi Maschas <alexi.maschas@gmail.com>
2013-04-22 01:31:22 -0400

    fixing gunicorn.sh

--- etc/gunicorn.sh
+++ etc/gunicorn.sh
@@ -4,9 +4,9 @@ LOGFILE=/var/log/rules/gunicorn.log
 LOGDIR=$(dirname $LOGFILE)
 NUM_WORKERS=3
 # user/group to run as
-USER=rules
-GROUP=rules
+USER=alexi
+GROUP=alexi
 cd /usr/local/django/rules/rules
 source /usr/local/django/rules/bin/activate
 test -d $LOGDIR || mkdir -p $LOGDIR
-exec /usr/local/django/rules/bin/gunicorn_django -w $NUM_WORKERS --user=$USER --group=$GROUP --log-level=debug --log-file=$LOGFILE 2>>$LOGFILE -b 0.0.0.0:8007
+exec /usr/local/django/rules/bin/gunicorn_django -w $NUM_WORKERS --user=$USER --group=$GROUP --log-level=debug --log-file=$LOGFILE 2>>$LOGFILE -b 0.0.0.0:8000
