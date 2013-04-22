5cbccd3be048f4a26640588ab9d4331fc0ae5204
Alexi Maschas <alexi.maschas@gmail.com>
2013-04-22 01:44:03 -0400

    fixing gunicorn.sh

--- etc/gunicorn.sh
+++ etc/gunicorn.sh
@@ -9,4 +9,4 @@ GROUP=alexi
 cd /usr/local/django/rules/rules
 source /usr/local/django/rules/bin/activate
 test -d $LOGDIR || mkdir -p $LOGDIR
-exec /usr/local/django/rules/bin/gunicorn_django -w $NUM_WORKERS --user=$USER --group=$GROUP --log-level=debug --log-file=$LOGFILE 2>>$LOGFILE -b 0.0.0.0:8000
+exec /usr/local/django/rules/bin/python /usr/local/django/rules/bin/gunicorn_django -w $NUM_WORKERS --user=$USER --group=$GROUP --log-level=debug --log-file=$LOGFILE -b 0.0.0.0:8000
