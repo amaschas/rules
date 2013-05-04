268ef6caeba1a00547f2598938e6d9edff7227a8
Alexi Maschas <alexi.maschas@gmail.com>
2013-05-04 12:41:29 -0400

    testing time bug

--- rules/tasks.py
+++ rules/tasks.py
@@ -112,7 +112,7 @@ def update_rule(rule, batch_size=5000):
                 except IndexError:
                   pass
 
-            # print index
+            print index
             # print date
             bulk_score.delay(deque(task_list))
             task_list.clear()
